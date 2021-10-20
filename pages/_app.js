1b84317cd3e7d66661898845d856871ca972fd6c
Sakshi Ranka <ranka.sakshi9@gmail.com>
2021-10-20 21:30:50 +0530

    fix page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useEffect, useState } from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Navigation from "../components/Navigation/Navigation";
+import { NavShowContext } from "../utils/context/navContext";
 import "../styles/globals.css";
 
-function useDelayUnmount(isMounted, delayTime) {
+function useDelayUnmount({ isMounted, setIsmounted }, delayTime) {
   const [shouldRender, setShouldRender] = useState(false);
 
   useEffect(() => {
@@ -13,6 +15,7 @@ function useDelayUnmount(isMounted, delayTime) {
     } else if (!isMounted && shouldRender) {
       timeoutId = setTimeout(() => {
         setShouldRender(false);
+        // setIsmounted(true);
       }, delayTime);
     }
     return () => clearTimeout(timeoutId);
@@ -22,25 +25,35 @@ function useDelayUnmount(isMounted, delayTime) {
 
 function MyApp({ Component, pageProps }) {
   const [isMounted, setIsmounted] = useState(true);
-  const shouldRenderChild = useDelayUnmount(isMounted, 500);
-
+  const shouldRenderChild = useDelayUnmount({ isMounted, setIsmounted }, 500);
+  const [showNav, setShowNav] = useState(true);
+  console.log({
+    Component,
+    isMounted,
+    shouldRenderChild,
+    route: useRouter().pathname,
+  });
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <main className="grid">
-        <Navigation setIsmounted={setIsmounted} />
-        {shouldRenderChild && (
-          <div
-            className={`sr-wrapper ${isMounted === false ? "sr_remove" : null}`}
-          >
-            <div className="sr-container">
-              <Component {...pageProps} />
+      <NavShowContext.Provider value={{ showNav, setShowNav }}>
+        <main className={`${showNav ? "grid_with_nav" : "grid_no_nav"}`}>
+          {showNav && <Navigation setIsmounted={setIsmounted} />}
+          {shouldRenderChild && (
+            <div
+              className={`sr-wrapper ${
+                isMounted === false ? "sr_remove" : null
+              }`}
+            >
+              <div className="sr-container">
+                <Component {...pageProps} />
+              </div>
             </div>
-          </div>
-        )}
-      </main>
+          )}
+        </main>
+      </NavShowContext.Provider>
     </>
   );
 }
