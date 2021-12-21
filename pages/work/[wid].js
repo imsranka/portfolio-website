7c8be4064d15ccadd2a85b45c2e7ecc159082bd5
imsranka <ranka.sakshi9@gmail.com>
2021-12-21 18:45:37 +0530

    individual project design

--- pages/work/[wid].js
+++ pages/work/[wid].js
@@ -1,11 +1,24 @@
 import { useRouter } from "next/dist/client/router";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { NavShowContext } from "../../utils/context/navContext";
 import pStyle from "../../styles/Project.module.css";
-
+import Logo from "../../components/Name/logo";
+import Image from "next/image";
+import hexOverlay from "../../assets/images/overlays-02.svg";
+import { projects } from "../../shared/data";
+function getProjectInfo(wid) {
+  console.log(wid);
+  return projects.find(
+    (element) =>
+      element.name.toLowerCase() === wid.replace(/-/g, " ").toLowerCase()
+  );
+}
 const Wid = () => {
   const router = useRouter();
+  const { wid } = router.query;
+  console.log(wid);
   const { showNav, setShowNav } = useContext(NavShowContext);
+  const projInfo = useMemo(() => getProjectInfo(wid), [wid]);
 
   useEffect(() => {
     setShowNav(false);
@@ -13,110 +26,72 @@ const Wid = () => {
       setShowNav(true);
     };
   }, []);
+
+  useEffect(() => {
+    // let x = projects.find(
+    //   (element) =>
+    //     element.name.toLowerCase() === wid.replace(/-/g, " ").toLowerCase()
+    // );
+    console.log(projInfo);
+  }, [projInfo]);
+
   return (
     <div
+      className={pStyle.project_wrapper}
       style={{
         color: "black",
         overflow: "auto",
         height: "100vh",
       }}
     >
-      <div className={pStyle.close_btn} onClick={() => router.push("/works")}>
+      <div className={pStyle.logo_wrapper}>
+        <Logo />
+      </div>
+
+      <div className={pStyle.close_btn} onClick={() => router.push("/work")}>
         X
       </div>
-      <h3 className={pStyle.banner}>{router.query.wid}</h3>
+      {/* <h3 className={pStyle.banner}>{router.query.wid}</h3> */}
       <article className={pStyle.outer_container}>
         <section className={pStyle.concept}>
-          <span className={pStyle.title}>Concept</span>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
+          {/* <Image src={hexOverlay} /> */}
+          <div className={`${pStyle.hero_bg}`}>
+            <div className={`${pStyle.title} ${pStyle.center}`}>
+              <span className={`${pStyle.text_overlay} ${pStyle.center}`}>
+                {projInfo.name}
+              </span>
+            </div>
           </div>
+
+          {projInfo && (
+            <div className={pStyle.content}>
+              <p>{projInfo.content[0]}</p>
+              <p>{projInfo.content[1]}</p>
+            </div>
+          )}
         </section>
-        <section className={pStyle.design}>
-          <span className={pStyle.title}>Design</span>
+        <aside>
+          <span className={pStyle.credits_main}>Credits</span>
           <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
+            {projInfo.credits.map((name, i) => (
+              <p key="i+name" className={pStyle.credit_name}>
+                {name}
+              </p>
+            ))}
           </div>
-        </section>
-        <section className={pStyle.concept}>
-          <span className={pStyle.title}>Concept</span>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
-          </div>
-        </section>
-        <section className={pStyle.design}>
-          <span className={pStyle.title}>Design</span>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
-          </div>
-        </section>
-        <section className={pStyle.concept}>
-          <span className={pStyle.title}>Concept</span>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
-          </div>
-        </section>
-        <section className={pStyle.design}>
-          <span className={pStyle.title}>Design</span>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
-          </div>
-        </section>
-        <section className={pStyle.concept}>
-          <span className={pStyle.title}>Concept</span>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
-          </div>
-        </section>
-        <section className={pStyle.design}>
-          <span className={pStyle.title}>Design</span>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nisi
-            sit amet augue tristique ullamcorper non quis orci. Aenean enim est,
-            laoreet fringilla tincidunt et, fringilla non tellus. Fusce molestie
-            ex at lacinia auctor. Proin elementum tortor vitae efficitur
-            porttitor. Morbi sed finibus nibh. Nullam posuere porta nunc vel
-            ullamcorper. Phasellus suscipit mi ac velit aliquet maximus.
-          </div>
-        </section>
+        </aside>
       </article>
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  console.log(context);
+  return {
+    props: {
+      wid: context.query.wid,
+    }, // will be passed to the page component as props
+  };
+}
 
 export default Wid;
