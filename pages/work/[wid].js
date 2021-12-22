600a3595cda32fdd997a32eea17ad013ca7bd58b
imsranka <ranka.sakshi9@gmail.com>
2021-12-22 19:37:58 +0530

    key bug fix

--- pages/work/[wid].js
+++ pages/work/[wid].js
@@ -6,6 +6,8 @@ import Logo from "../../components/Name/Logo";
 import Image from "next/image";
 import hexOverlay from "../../assets/images/overlays-02.svg";
 import { projects } from "../../shared/data";
+import Head from "next/head";
+
 function getProjectInfo(wid) {
   console.log(wid);
   return projects.find(
@@ -13,6 +15,7 @@ function getProjectInfo(wid) {
       element.name.toLowerCase() === wid.replace(/-/g, " ").toLowerCase()
   );
 }
+
 const Wid = () => {
   const router = useRouter();
   const { wid } = router.query;
@@ -36,60 +39,65 @@ const Wid = () => {
   }, [projInfo]);
 
   return (
-    <div
-      className={pStyle.project_wrapper}
-      style={{
-        color: "black",
-        overflow: "auto",
-        height: "100vh",
-      }}
-    >
-      <div className={pStyle.logo_wrapper}>
-        <Logo />
-      </div>
+    <>
+      <Head>
+        <title>SR | {projInfo.name}</title>
+      </Head>
+      <div
+        className={pStyle.project_wrapper}
+        style={{
+          color: "black",
+          overflow: "auto",
+          height: "100vh",
+        }}
+      >
+        <div className={pStyle.logo_wrapper}>
+          <Logo />
+        </div>
 
-      <div className={pStyle.close_btn} onClick={() => router.push("/work")}>
-        X
-      </div>
-      {/* <h3 className={pStyle.banner}>{router.query.wid}</h3> */}
-      <article className={pStyle.outer_container}>
-        <section className={pStyle.concept}>
-          {/* <Image src={hexOverlay} /> */}
-          <div className={`${pStyle.hero_bg}`}>
-            <div className={`${pStyle.title} ${pStyle.center}`}>
-              <span className={`${pStyle.text_overlay} ${pStyle.center}`}>
-                {projInfo.name}
-              </span>
+        <div className={pStyle.close_btn} onClick={() => router.push("/work")}>
+          X
+        </div>
+        {/* <h3 className={pStyle.banner}>{router.query.wid}</h3> */}
+        <article className={pStyle.outer_container}>
+          <section className={pStyle.concept}>
+            {/* <Image src={hexOverlay} /> */}
+            <div className={`${pStyle.hero_bg}`}>
+              <div className={`${pStyle.title} ${pStyle.center}`}>
+                <span className={`${pStyle.text_overlay} ${pStyle.center}`}>
+                  {projInfo.name}
+                </span>
+              </div>
             </div>
-          </div>
 
-          {projInfo && (
-            <div className={pStyle.content}>
-              <p>{projInfo.content[0]}</p>
-              <p>{projInfo.content[1]}</p>
-            </div>
-          )}
-        </section>
-        <section>
-          {projInfo.images.map((img, i) => (
-            <div key={i + img} className={pStyle.image_container}>
-              <img src={img.path} alt={img.alt} width="100%" />
-              <p className={pStyle.image_caption}>{img.caption}</p>
-            </div>
-          ))}
-        </section>
-        <aside>
-          <span className={pStyle.credits_main}>Credits</span>
-          <div>
-            {projInfo.credits.map((name, i) => (
-              <p key="i+name" className={pStyle.credit_name}>
-                {name}
-              </p>
+            {projInfo && (
+              <div className={pStyle.content}>
+                <p>{projInfo.content[0]}</p>
+                <p>{projInfo.content[1]}</p>
+              </div>
+            )}
+          </section>
+          <section>
+            {projInfo.images?.map((img, i) => (
+              <div key={i + img} className={pStyle.image_container}>
+                <img src={img.path} alt={img.alt} width="100%" />
+                <p className={pStyle.image_caption}>{img.caption}</p>
+              </div>
             ))}
-          </div>
-        </aside>
-      </article>
-    </div>
+          </section>
+          <aside>
+            <span className={pStyle.credits_main}>Credits</span>
+            <div>
+              {projInfo.credits.map((name, i) => (
+                <p key={i + name} className={pStyle.credit_name}>
+                  {name}
+                </p>
+              ))}
+            </div>
+          </aside>
+        </article>
+      </div>
+    </>
   );
 };
 
